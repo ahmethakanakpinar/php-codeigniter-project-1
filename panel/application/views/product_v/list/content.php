784d7281723ba58cd3e452f3fd7270cebd2239b1
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-03-23 15:37:08 +0300

    ürünlerin sıralanacağı tablo düzeltildi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,36 @@
-PRODUCT
+<section class="app-content">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="m-b-lg">Ürünler</h4>
+			</div><!-- END column -->
+			<div class="col-md-12">
+				<div class="widget p-lg">
+					<table class="table">
+						<thead>
+                            <tr>
+                                <th>#id</th>
+                                <th>url</th>
+                                <th>Başlık</th>
+                                <th>Açıklama</th>
+                                <th>Durumu</th>
+                                <th>İşlem</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>monitor-askisi</td>
+                                <td>Monitör Askısı</td>
+                                <td>360 derece kullanılabilen monitör askısıdır.</td>
+                                <td>1/0</td>
+                                <td>
+                                    <a class="btn btn-danger btn-outline btn-sm" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Sil</a>
+                                    <a class="btn btn-primary btn-outline btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Düzenle</a>
+                                </td>
+                            </tr>
+                        </tbody>
+					</table>
+				</div><!-- .widget -->
+			</div><!-- END column -->
+		</div><!-- .row -->
+	</section><!-- #dash-content -->
