35461eb1a923ab05b52138bc9e7ac3d73203e02f
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-03-23 16:03:02 +0300

    ürünler tablosunun toggle eklendi ve alert eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,10 +1,16 @@
 <section class="app-content">
 		<div class="row">
 			<div class="col-md-12">
-				<h4 class="m-b-lg">Ürünler</h4>
+				<h4 class="m-b-lg">
+                    Ürünler
+                    <a class="btn btn-primary btn-outline pull-right btn-xs" href="#"><i class="fa fa-plus" aria-hidden="true"></i> Ekle</a>
+                </h4>
 			</div><!-- END column -->
 			<div class="col-md-12">
 				<div class="widget p-lg">
+                <div class="alert alert-info text-center">
+                    <p>Burada herhangi bir veri bulunamamaktadır. Eklemek için <a href="#">Tıklayınız</a></p>
+                </div>
 					<table class="table">
 						<thead>
                             <tr>
@@ -22,10 +28,25 @@
                                 <td>monitor-askisi</td>
                                 <td>Monitör Askısı</td>
                                 <td>360 derece kullanılabilen monitör askısıdır.</td>
-                                <td>1/0</td>
+                                <td>
+                                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+							    </td>
                                 <td>
                                     <a class="btn btn-danger btn-outline btn-sm" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Sil</a>
-                                    <a class="btn btn-primary btn-outline btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Düzenle</a>
+                                    <a class="btn btn-info btn-outline btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Düzenle</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>#1</td>
+                                <td>monitor-askisi</td>
+                                <td>Monitör Askısı</td>
+                                <td>360 derece kullanılabilen monitör askısıdır.</td>
+                                <td>
+                                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+							    </td>
+                                <td>
+                                    <a class="btn btn-danger btn-outline btn-sm" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Sil</a>
+                                    <a class="btn btn-info btn-outline btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Düzenle</a>
                                 </td>
                             </tr>
                         </tbody>
