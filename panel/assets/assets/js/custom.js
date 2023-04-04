fc125082fac77de439005482c8a2760967d219a3
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-04 12:01:30 +0300

    sweetalert butonu kodları girildi artık sil e tıklayınca direk silmicek uyarı vericek uyarıdan da evet sil yaparsan silinecek

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-   
-    swal({
-        title: "Error!",
-        text: "Do yo want",
-        type: "error",
-        confirmButtonText: "Cool"
+    $(".remove-btn").click(function (e)
+    {
+        var $data_url = $(this).data("url");
+        swal({
+            title: "Emin misiniz?",
+            text: "Bu işlemi geri alamayacaksınız!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Evet, Sil!",
+            cancelButtonText: "Hayır"
+        }).then(function(result)
+        {
+            if(result.value)
+            {
+                window.location.href = $data_url;
+            }
+        });
     })
-
 })
