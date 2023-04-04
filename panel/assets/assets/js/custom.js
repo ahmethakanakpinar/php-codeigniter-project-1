36018bdd440c9903cfa4d2b43d6eeb061d4d769e
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-04 14:21:30 +0300

    switch düzeltildi artık değiştiği an veritabanında değişecektir.

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".remove-btn").click(function (e)
+    $(".remove-btn").click(function ()
     {
         var $data_url = $(this).data("url");
         swal({
@@ -18,5 +18,15 @@ $(document).ready(function(){
                 window.location.href = $data_url;
             }
         });
+    })
+    $(".isActive").change(function()
+    {
+        var $data = $(this).prop("checked");
+        var $data_url = $(this).data("url");
+
+        if(typeof $data !== "undefined" && typeof $data_url !== "undefined")
+        {
+            $.post($data_url, { data : $data}, function (response) {});
+        }
     })
 })
