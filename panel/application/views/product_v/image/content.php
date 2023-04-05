407c418440b037485f4c3e743d40b0c3f3b0be06
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-05 14:27:43 +0300

    image_form da ki tablonun css leri ile oynandı

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,12 +1,6 @@
 <section class="app-content">
 		<div class="row">
 			<div class="col-md-12">
-				<h4 class="m-b-lg">
-                    Fotoğraf Ekle
-                    <a class="btn btn-primary btn-outline pull-right btn-xs" href="#"><i class="fa fa-plus" aria-hidden="true"></i> Ekle</a>
-                </h4>
-			</div><!-- END column -->
-			<div class="col-md-12">
 				<div class="widget p-lg">
 					<div class="widget-body">
 						<form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
@@ -29,7 +23,7 @@
 			<div class="col-md-12">
 				<div class="widget p-lg">
 					<div class="widget-body">
-						<table class="table table-bordered table-striped table-hover">
+						<table class="table table-bordered table-striped table-hover picture_list">
 							<thead>
 								<th>#id</th>
 								<th>Görsel</th>
@@ -39,11 +33,11 @@
 							</thead>
 							<tbody>
 								<tr>
-									<td>#1</td>
-									<td><img width="30" class="img-responsive" src="https://s3-eu-west-1.amazonaws.com/tpd/logos/5be01d787b5e5b0001ebb6bb/0x0.png" alt=""></td>
+									<td class="w100 text-center">#1</td>
+									<td class="w100 text-center"><img width="30" class="img-responsive" src="https://s3-eu-west-1.amazonaws.com/tpd/logos/5be01d787b5e5b0001ebb6bb/0x0.png" alt=""></td>
 									<td>deneme.jpeg</td>
-									<td><input class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo (true) ? "checked": "" ?> /></td>
-									<td><button class="btn btn-danger btn-outline btn-sm remove-btn"><i class="fa fa-trash" aria-hidden="true"></i> Sil</button></td>
+									<td class="w100 text-center"><input class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo (true) ? "checked": "" ?> /></td>
+									<td class="w100 text-center"><button class="btn btn-danger btn-outline btn-block btn-sm remove-btn"><i class="fa fa-trash" aria-hidden="true"></i> Sil</button></td>
 								</tr>
 							</tbody>
 						</table>
