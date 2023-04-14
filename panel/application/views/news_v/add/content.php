ae4bdfbe38dad093ae555ef32e91aa626769b479
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-14 16:42:12 +0300

    haberlerin add kısmı yapıldı

--- panel/application/views/news_v/add/content.php
+++ panel/application/views/news_v/add/content.php
@@ -21,6 +21,26 @@
 								<label for="exampleInputPassword1">Açıklama</label>
 								<textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
 							</div>
+							<div class="form-group">
+								<label for="control-demo-6" class="">Haberin Türü</label>
+								<div id="control-demo-6" class="">
+									<select class="form-control">
+										<option value="image">Resim</option>
+										<option value="movie">Video</option>
+									</select>
+								</div>
+							</div><!-- .form-group -->
+							<div class="form-group">
+								<label for="exampleInputFile">Görsel Seçiniz</label>
+								<input type="file" id="exampleInputFile" class="form-control" name="img_url">
+							</div><!-- .form-group -->
+							<div class="form-group <?php echo isset($form_error)? "has-error":""  ?>">
+								<label for="title">Video Url</label>
+								<input type="text" class="form-control" id="title" name="video_url" placeholder="Video bağlantısını buraya yapıştırın">
+								<?php if(isset($form_error)): ?>
+								<small>Hata</small>
+								<?php endif; ?>
+							</div>
 							<button type="submit" class="btn btn-primary btn-md btn-outline">Submit</button>
 							<a href="<?php echo base_url("$viewTitle") ?>" class="btn btn-danger btn-md btn-outline">İptal</a>
 						</form>
