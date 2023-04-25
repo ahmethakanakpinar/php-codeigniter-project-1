7c498bb62cfb8f161cbfa259d6c4aac52819b959
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-25 10:51:22 +0300

    references update_form kısmı yazıldı ve update content kısmı düzeltildi

--- panel/application/controllers/References.php
+++ panel/application/controllers/References.php
@@ -116,6 +116,106 @@ class References extends CI_Controller{
         }
       
     }
+    public function update_form($id)
+    {
+       $item = $this->reference_model->get(
+          array(
+            "id" => $id
+          )
+        );
+        $viewData = new stdClass();
+        $viewData->viewTitle = $this->viewTitle;
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->item = $item;
+        $viewData->subViewFolder = "update";
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+    public function update()
+    {
+        $this->load->library("form_validation");
+        if($_FILES["img_url"]["name"] == "")
+        {
+            $alert = array(
+                "title" => "İşlem Başarısız",
+                "text" => "Lütfen bir görsel Seçiniz!",
+                "type" => "error"
+            );
+            $this->session->set_flashdata("alert", $alert);
+            redirect(base_url("{$this->viewTitle}/new_form"));
+            die();
+        }
+        $this->form_validation->set_rules("title","Başlık","required|trim");
+        $this->form_validation->set_message(
+            array(
+                "required" => "{field} alanı doldurulmalıdır!"
+            )
+        );
+        $validate = $this->form_validation->run();
+        if($validate)
+        {
+            $file_name = CharConvert(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)). "." .pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
+            $config["allowed_types"] = "jpg|jpeg|png";
+            $config["upload_path"] = "uploads/$this->viewFolder/";
+            $config["file_name"] = $file_name;
+            $this->load->library("upload",$config);
+            $upload = $this->upload->do_upload("img_url");
+            if($upload)
+            {
+                $uploaded_file = $this->upload->data("file_name");
+                $insert = $this->reference_model->add(
+                    array(
+                        "url"           => CharConvert($this->input->post("title")),
+                        "title"         => $this->input->post("title"),
+                        "description"   => $this->input->post("description"),
+                        "img_url"       => $uploaded_file,
+                        "rank"          =>0,
+                        "isActive"      =>0,
+                        "createdAt"     =>date("Y-m-d H:i:s")
+        
+                    )
+                );
+                if($insert)
+                {
+                    $alert = array(
+                        "title" => "İşlem Başarılı",
+                        "text" => "Kayıt başarılı bir şekilde eklendi",
+                        "type"  => "success"
+                    );
+                }
+                else 
+                {
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text" => "Kayıt Ekleme sırasında bir problem oluştu",
+                        "type"  => "error"
+                    );
+                }
+            }
+            else
+            {
+                $alert = array(
+                    "title" => "İşlem Başarısız",
+                    "text" => "Görsel Yükleme de problem yaşandı!",
+                    "type" => "error"
+                );
+                $this->session->set_flashdata("alert", $alert);
+                redirect(base_url("{$this->viewTitle}/new_form"));
+                die();
+            }
+            $this->session->set_flashdata("alert", $alert);
+            redirect(base_url("{$this->viewTitle}"));
+        }
+        else
+        {
+            $viewData = new stdClass();
+            $viewData->viewTitle = $this->viewTitle;
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "add";
+            $viewData->form_error = true;
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+      
+    }
 
 }
 
--- panel/application/views/references_v/update/content.php
+++ panel/application/views/references_v/update/content.php
@@ -21,49 +21,17 @@
 								<label for="exampleInputPassword1">Açıklama</label>
 								<textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"><?php echo $item->description ?></textarea>
 							</div>
-							<div class="form-group ">
-								<label for="control-demo-6" class="">Haberin Türü</label>
-								<div id="control-demo-6" class="">
-									<select class="form-control news_type_select" name="news_type">
-										<?php if($form_error): ?>
-											<option value="image" <?php echo ($news_type == "image") ? "selected" : "";  ?>>Resim</option>
-											<option value="movie" <?php echo ($news_type == "movie") ? "selected" : "";  ?>>Video</option>
-										<?php else: ?>
-											<option value="image" <?php echo ($item->news_type == "image") ? "selected":""; ?>>Resim</option>
-											<option value="movie" <?php echo ($item->news_type == "movie") ? "selected":""; ?>>Video</option>
-										<?php endif; ?>
-									</select>
-								</div>
-							</div><!-- .form-group -->
-							<?php if(isset($form_error)): ?>
-								<div class="form-group image_upload_container" style="display: <?php echo ($item->news_type == "image") ? "block" : "none"; echo $news_type == "image" ? "block" : "none" ?>">
-									<label for="exampleInputFile">Görsel Seçiniz</label>
-									<input type="file" id="exampleInputFile" class="form-control" name="img_url">
-								</div><!-- .form-group -->
-								<div class="form-group video_url_container <?php echo isset($form_error)? "has-error":""  ?>" style="display: <?php echo $news_type == "movie" ? "block" : "none" ?>">
-									<label for="title">Video Url</label>
-									<input type="text" class="form-control" id="title" name="video_url" placeholder="Video bağlantısını buraya yapıştırın">
-									<?php if(isset($form_error)): ?>
-									<small>Hata</small>
-									<?php endif; ?>
-								</div>
-							<?php else: ?>
 								<div class="row">
 									<div class="col-md-2 image_upload_container">
 										<img src="<?php echo base_url("uploads/{$viewFolder}/{$item->img_url}") ?>" alt="<?php $item->img_url ?>">
 									</div>
 									<div class="col-md-10">
-										<div class="form-group image_upload_container" style="display: <?php  echo ($item->news_type == "image") ? "block" : "none";  ?>">
+										<div class="form-group image_upload_container">
 											<label for="exampleInputFile">Görsel Seçiniz</label>
 											<input type="file" id="exampleInputFile" class="form-control" name="img_url" value="<?php $item->img_url ?>">
 										</div><!-- .form-group -->
 									</div>
 									</div>
-								<div class="form-group video_url_container <?php echo isset($form_error)? "has-error":""  ?>" style="display: <?php  echo ($item->news_type == "movie") ? "block" : "none";  ?>">
-									<label for="title">Video Url</label>
-									<input type="text" class="form-control" id="title" name="video_url" placeholder="Video bağlantısını buraya yapıştırın" value="<?php echo $item->video_url ?>">
-								</div>
-							<?php endif; ?>
 							<button type="submit" class="btn btn-primary btn-md btn-outline">Güncelle</button>
 							<a href="<?php echo base_url("$viewTitle") ?>" class="btn btn-danger btn-md btn-outline">İptal</a>
 						</form>
