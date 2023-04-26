3dcb908eac587f8a027c273226f65d4e0c08d036
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-04-26 10:49:08 +0300

    markalar list de ufak still değişiklikleri

--- panel/application/views/brands_v/list/content.php
+++ panel/application/views/brands_v/list/content.php
@@ -28,17 +28,17 @@
                             <tbody class="sortable" data-url="<?php echo base_url("$viewTitle/rankSetter") ?>">
                                 <?php foreach($items as $item): ?>
                                 <tr id = "ord-<?php echo $item->id ?>">
-                                    <td><i class="fa fa-reorder" aria-hidden="true"></i></td>
-                                    <td><?php echo $item->id ?></td>
+                                    <td class="w-100"><i class="fa fa-reorder" aria-hidden="true"></i></td>
+                                    <td class="w-100"><?php echo $item->id ?></td>
                                     <td><?php echo $item->title ?></td>
-                                    <td class="text-center">
+                                    <td class="text-center w-100">
                                             <img width="75" class="img-fluid img-rounded" src="<?php echo "uploads/{$viewFolder}/{$item->img_url}" ?>" alt="<?php $item->img_url ?>">
                                     </td>
-                                    <td>
+                                    <td class="text-center w-100">
                                         <input data-url="<?php echo base_url("$viewTitle/isActiveSetter/$item->id"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo ($item->isActive) ? "checked": "" ?> />
                                     </td>
-                                    <td>
-                                        <button  class="btn btn-danger btn-outline btn-sm remove-btn" data-url="<?php echo base_url("$viewTitle/delete/$item->id")?>"><i class="fa fa-trash" aria-hidden="true"></i> Sil</button>
+                                    <td class="text-center w-200">
+                                        <button class="btn btn-danger btn-outline btn-sm remove-btn" data-url="<?php echo base_url("$viewTitle/delete/$item->id")?>"><i class="fa fa-trash" aria-hidden="true"></i> Sil</button>
                                         <a class="btn btn-info btn-outline btn-sm" href="<?php echo base_url("$viewTitle/update_form/$item->id")?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Düzenle</a>
                                     </td>
                                 </tr>
